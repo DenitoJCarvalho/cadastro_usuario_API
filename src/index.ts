5bba64a52abf41d5930da952fa6c7a3539adfece
Dennis <dennisjcarvalho@gmail.com>
2023-10-05 18:05:24 -0300

    feat: cors configurado cors para nao ocorrer bloqueio por cross brownser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 
 import { normalizePort } from './server/server';
 import { dotenv } from './environment/env';
@@ -9,6 +9,19 @@ import 'reflect-metadata';
 
 const app = express();
 const port = normalizePort(dotenv.PORT || '9292');
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: [
+    'Content-Type', 'Authorizathion'
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true
+}));
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running in dev localhost:${port}`);
